ae8bc90608142a41e01f1bb058355da62a31eb44
Jason Priebe <jason.priebe@gmail.com>
2016-04-13 09:26:16 -0400

    switched to lorempixel for images

--- Resources/ui/UIViewsGalleryViewDemoWindow.js
+++ Resources/ui/UIViewsGalleryViewDemoWindow.js
@@ -10,110 +10,29 @@ function UIViewsGalleryViewDemoWindow ()
 	var margin = 10;
 	var imgw = 240;
 	
-	var images = [
-		{
-   			headline: 'Barack Obama at UNC-CH',
- 			renditions: [
-       			{
-					width: 120,
-					height: 90,
-					url: 'http://wwwcache.wral.com/asset/news/local/2012/04/25/11020841/11020841-1335347760-120x90.jpg'
-				},
-				{
-				    width: 640,
-				    height: 480,
-				    url: 'http://wwwcache.wral.com/asset/news/local/2012/04/25/11020841/11020841-1335347760-640x480.jpg'
-				}
-			]
-		},
-		{
-   			headline: 'Tar Heel Nation',
- 			renditions: [
-       			{
-					width: 120,
-					height: 90,
-					url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2012/02/29/10796821/10796821-1330708109-120x90.jpg'
-				},
-				{
-				    width: 640,
-				    height: 480,
-				    url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2012/02/29/10796821/10796821-1330708109-640x480.jpg'
-				}
-			]
-		},
-		{
-   			headline: 'Tyler Hansbrough Dunk',
- 			renditions: [
-       			{
-					width: 180,
-					height: 135,
-					url: 'http://wwwcache.wralsportsfan.com/asset/2008/03/24/2619315/2619315-1206366712-180x135.jpg'
-				},
-				{
-				    width: 645,
-				    height: 483,
-				    url: 'http://wwwcache.wralsportsfan.com/asset/2008/03/24/2619315/2619315-1206366712-645x483.jpg'
-				}
-			]
-		},
-		{
-   			headline: 'Harrison Barnes',
- 			renditions: [
-       			{
-					width: 78,
-					height: 135,
-					url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2012/03/09/10836365/_jtr4701-78x135.jpg'
-				},
-				{
-				    width: 262,
-				    height: 450,
-				    url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2012/03/09/10836365/_jtr4701-262x450.jpg'
-				}
-			]
-		},
-		{
-   			headline: 'John Henson Dunk',
- 			renditions: [
-       			{
-					width: 120,
-					height: 90,
-					url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2011/03/20/9299190/9299190-1302121528-120x90.jpg'
-				},
-				{
-				    width: 640,
-				    height: 480,
-				    url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2011/03/20/9299190/9299190-1302121528-640x480.jpg'
-				}
-			]
-		},
-		{
-   			headline: 'Ty Lawson',
- 			renditions: [
-       			{
-					width: 120,
-					height: 90,
-					url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2009/03/21/4788622/4788622-1245789984-120x90.jpg'
-				},
-				{
-				    width: 640,
-				    height: 480,
-				    url: 'http://wwwcache.wralsportsfan.com/asset/colleges/unc/2009/03/21/4788622/4788622-1245789984-640x480.jpg'
-				}
-			]
-		}
-	];
+	var images = [];
 	
-	// repeat the images to show a nice full gallery...
-	var newimages = [];
 	for (var i = 0; i < 10; i++)
 	{
-		for (var j = 0; j < images.length; j++)
-		{
-			newimages.push (images[j]);
-		}
+		var i1 = i + 1;
+		
+		var img = {
+			headline: 'City picture ' + i1 + ' (courtesy of lorempixel.com)',
+			renditions: [
+       			{
+					width: 240,
+					height: 180,
+					url: 'http://lorempixel.com/240/180/city/' + i1
+				},
+				{
+				    width: 640,
+				    height: 480,
+				    url: 'http://lorempixel.com/640/480/city/' + i1
+				}
+			]
+		};
+		images.push (img);
 	}
-	images = newimages;
-	
 	
 	var contentview = Ti.UI.createView ({
 		top: margin,
